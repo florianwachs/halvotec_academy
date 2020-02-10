65bd06a66895c7eb6bb516eadcb9951897dad5e6
Florian Wachs <florian.wachs@live.de>
2020-02-10 13:37:11 +0100

    dev tools

--- Sessions/React State Management/01_reduxthunk/src/store/store.ts
+++ Sessions/React State Management/01_reduxthunk/src/store/store.ts
@@ -1,12 +1,12 @@
 import { createStore, combineReducers, applyMiddleware, DeepPartial } from "redux";
 import thunk from "redux-thunk";
 import { IUsersState, userReducer } from "./users/reducers";
-
+import { composeWithDevTools } from "redux-devtools-extension";
 export interface RootState {
   users: IUsersState;
 }
 
 export const rootStore = createStore(
   combineReducers<RootState>({ users: userReducer }),
-  applyMiddleware(thunk)
+  composeWithDevTools(applyMiddleware(thunk))
 );
